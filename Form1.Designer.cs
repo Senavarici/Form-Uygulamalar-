ef884d1d2213e7d6971fb68dc5772cfd6a7cb3ea
Senavarici <100046373+Senavarici@users.noreply.github.com>
2022-04-11 12:42:22 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,434 @@
+﻿
+namespace marangoz1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMusteri = new System.Windows.Forms.TextBox();
+            this.cmbMasa = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cmbAgac = new System.Windows.Forms.ComboBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.txtYaricap = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEn = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txyBoy = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtTutar = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Müşteri Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Masa Türü";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Çekmece Sayısı";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 317);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ağaç Türü";
+            // 
+            // txtMusteri
+            // 
+            this.txtMusteri.Location = new System.Drawing.Point(142, 51);
+            this.txtMusteri.Name = "txtMusteri";
+            this.txtMusteri.Size = new System.Drawing.Size(100, 20);
+            this.txtMusteri.TabIndex = 4;
+            // 
+            // cmbMasa
+            // 
+            this.cmbMasa.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMasa.FormattingEnabled = true;
+            this.cmbMasa.Location = new System.Drawing.Point(142, 92);
+            this.cmbMasa.Name = "cmbMasa";
+            this.cmbMasa.Size = new System.Drawing.Size(100, 21);
+            this.cmbMasa.TabIndex = 7;
+            this.cmbMasa.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(569, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmbAgac
+            // 
+            this.cmbAgac.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbAgac.FormattingEnabled = true;
+            this.cmbAgac.Location = new System.Drawing.Point(140, 309);
+            this.cmbAgac.Name = "cmbAgac";
+            this.cmbAgac.Size = new System.Drawing.Size(100, 21);
+            this.cmbAgac.TabIndex = 9;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(43, 15);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(31, 17);
+            this.radioButton1.TabIndex = 11;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(80, 15);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(31, 17);
+            this.radioButton2.TabIndex = 12;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(116, 15);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(31, 17);
+            this.radioButton3.TabIndex = 13;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(19, 38);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(31, 17);
+            this.radioButton4.TabIndex = 14;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(56, 38);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(31, 17);
+            this.radioButton5.TabIndex = 15;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(93, 39);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(31, 17);
+            this.radioButton6.TabIndex = 16;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "6";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton7);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton6);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Location = new System.Drawing.Point(140, 206);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(147, 67);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(6, 15);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(31, 17);
+            this.radioButton7.TabIndex = 30;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "0";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // txtYaricap
+            // 
+            this.txtYaricap.Location = new System.Drawing.Point(142, 134);
+            this.txtYaricap.Name = "txtYaricap";
+            this.txtYaricap.Size = new System.Drawing.Size(100, 20);
+            this.txtYaricap.TabIndex = 19;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Yarıçap";
+            // 
+            // txtEn
+            // 
+            this.txtEn.Location = new System.Drawing.Point(142, 134);
+            this.txtEn.Name = "txtEn";
+            this.txtEn.Size = new System.Drawing.Size(102, 20);
+            this.txtEn.TabIndex = 21;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(37, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "En";
+            // 
+            // txyBoy
+            // 
+            this.txyBoy.Location = new System.Drawing.Point(142, 169);
+            this.txyBoy.Name = "txyBoy";
+            this.txyBoy.Size = new System.Drawing.Size(102, 20);
+            this.txyBoy.TabIndex = 23;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(37, 176);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(25, 13);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Boy";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(123, 276);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(164, 13);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Her çekmece için 40 TL eklenir !!";
+            // 
+            // txtTutar
+            // 
+            this.txtTutar.Location = new System.Drawing.Point(483, 63);
+            this.txtTutar.Name = "txtTutar";
+            this.txtTutar.Size = new System.Drawing.Size(100, 20);
+            this.txtTutar.TabIndex = 27;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(416, 66);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(44, 13);
+            this.label10.TabIndex = 28;
+            this.label10.Text = "TUTAR";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(732, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 39);
+            this.button2.TabIndex = 29;
+            this.button2.Text = "ÇIKIŞ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridView1.Location = new System.Drawing.Point(293, 169);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(580, 197);
+            this.dataGridView1.TabIndex = 30;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "İsim";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Masa Türü";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Alan";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Çekmece";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Ağaç Türü";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Tutar";
+            this.Column6.Name = "Column6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(885, 378);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtTutar);
+            this.Controls.Add(this.txyBoy);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtEn);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtYaricap);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cmbAgac);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbMasa);
+            this.Controls.Add(this.txtMusteri);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label8);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMusteri;
+        private System.Windows.Forms.ComboBox cmbMasa;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cmbAgac;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtYaricap;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txyBoy;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtTutar;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace marangoz1
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            visible();
+            switch (cmbMasa.Text)
+            {
+                case "Yuvarlak":
+                    txtYaricap.Visible = true;
+                    label5.Visible = true;
+                    break;
+                case "Dikdörtgen":
+                    txtEn.Visible = true;
+                    txyBoy.Visible = true;
+                    label6.Visible = true;
+                    label7.Visible = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            visible();
+            
+            cmbMasa.Items.Add("Yuvarlak");
+            cmbMasa.Items.Add("Dikdörtgen");
+
+            cmbAgac.Items.Add("Kavak");
+            cmbAgac.Items.Add("Ceviz (+ 150 TL)");
+            cmbAgac.Items.Add("Meşe (+ 250 TL)");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             double tutar = 0;
+             double yaricap , alan = 1 , en , boy;
+             int cekmece = 0;
+                
+             if(cmbMasa.SelectedIndex == 0) {
+                 yaricap = Convert.ToDouble(txtYaricap.Text);
+                 alan = 3.14 * yaricap * yaricap;
+             }
+
+             else if (cmbMasa.SelectedIndex == 1) {
+                 en = Convert.ToDouble(txtEn.Text);
+                 boy = Convert.ToDouble(txyBoy.Text);
+                 alan = en * boy;                 
+             }
+                     
+             if (alan <= 2.5) {               
+                 tutar += 300;
+             }
+
+             else if (alan <= 4.5) {               
+                 tutar += 400;
+             }
+             else
+                 MessageBox.Show("4.5 metre kareden büyük alan hesabı yapılamaz", "HATA");
+
+
+             if (radioButton7.Checked == true)
+                tutar = tutar + 0;
+
+             else if(radioButton1.Checked == true) {               
+                cekmece = 1;
+                tutar = tutar + 40;
+             }
+             else if (radioButton2.Checked == true) {
+                cekmece = 2;
+                tutar = tutar + 80;
+             }
+             else if (radioButton3.Checked == true) {              
+                cekmece = 3;
+                tutar = tutar + 120;
+             }
+             else if (radioButton4.Checked == true) {             
+                cekmece = 4;
+                tutar = tutar + 160;
+             }
+             else if (radioButton5.Checked == true) {                
+                cekmece = 5;
+                tutar = tutar + 200;
+             }
+             else if (radioButton6.Checked == true) {               
+                cekmece = 6;
+                tutar = tutar + 240;
+             }
+                
+
+                      
+             if (cmbAgac.Text == "Ceviz (+ 150 TL)") {                
+                tutar += 150;
+             }
+             else if (cmbAgac.Text == "Meşe (+ 250 TL)") {                
+                tutar += 250;
+             }
+             txtTutar.Text = tutar.ToString();
+
+
+             var index = dataGridView1.Rows.Add();
+             dataGridView1.Rows[index].Cells["Column1"].Value = txtMusteri.Text;
+             dataGridView1.Rows[index].Cells["Column2"].Value = cmbMasa.Text;
+             dataGridView1.Rows[index].Cells["Column3"].Value = alan.ToString();
+             dataGridView1.Rows[index].Cells["Column4"].Value = cekmece.ToString();
+             dataGridView1.Rows[index].Cells["Column5"].Value = cmbAgac.Text;
+             dataGridView1.Rows[index].Cells["Column6"].Value = tutar.ToString();
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void visible()
+        {
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            txtYaricap.Visible = false;
+            txtEn.Visible = false;
+            txyBoy.Visible = false;
+        }
+
+        
+    }
+}
